9af4f4712786e7a616a1724c215b71e01d443583
Romain BERNARD <30secondstodraw@gmail.com>
2025-02-06 19:48:11 +0100

    update prefabs and refactor spawn system to integrate pathing components

--- Assets/ECS/System/SpawnEntitySystem.cs
+++ Assets/ECS/System/SpawnEntitySystem.cs
@@ -1,78 +1,79 @@
+using System;
 using ECS.Components;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEngine;
 
 namespace ECS.System
 {
     [UpdateAfter(typeof(RandomizeColourSystem))] //Update after to have the right colouring
     partial struct SpawnEntitySystem : ISystem
     {
+        private EntityQuery _spawnerQuery;
+        // private EntityQuery _spawnOnceSpawnerQuery;
         
-        [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-        }
+            //Query used to look for spawners containing all the data we require to create a coloured entity moving over a given path
+            _spawnerQuery = state.EntityManager.CreateEntityQuery(ComponentType.ReadOnly<EntityPrefab>(), 
+                                                                  ComponentType.ReadOnly<LocalTransform>(),
+                                                                  ComponentType.ReadOnly<ColourComponent>(), 
+                                                                  ComponentType.ReadOnly<PathingComponent>(), 
+                                                                  ComponentType.ReadOnly<SpawnerMarker>(), 
+                                                                  ComponentType.ReadWrite<ShouldSpawnOnceMarker>());
 
-        [BurstCompile]
-        public void OnUpdate(ref SystemState state)
-        {
-            
-            //Handle car spawn on startup
-            foreach ((var entityPrefab, var spawnerTransform, var spawnerColour) in
-                     SystemAPI.Query<RefRW<EntityPrefab>, RefRO<LocalTransform>, RefRO<ColourComponent>>()
-                              .WithAll<SpawnerMarker, ShouldSpawnOnceMarker>())
-            {
-                CreateAndUpdateEntity(ref state, entityPrefab, spawnerTransform, spawnerColour);
-            }
-            
-            //Disable the "spawn once" marker to prevent spawning on every subsequent frame
-            // Debug.LogWarning("Disable spawn once marker");
-            EntityQueryBuilder queryBuilder = new EntityQueryBuilder(Allocator.Temp).WithAll<SpawnerMarker, ShouldSpawnOnceMarker>();
-            EntityQuery entityQuery = queryBuilder.Build(state.EntityManager);
-            if(!entityQuery.IsEmpty)
-                state.EntityManager.SetComponentEnabled<ShouldSpawnOnceMarker>(entityQuery, false);
-            entityQuery.Dispose();
-            queryBuilder.Dispose();
-            // TODO : check optimization of instantiating Builder and Query here. Builder is a ref so it seems fine ?
-            
-            // TODO: Handle car spawn on button hit
-            // foreach ((RefRW<SpawnerReference> spawnerReference, RefRO<SpawnCarShortcut> spawnCarShortcut) in
-            //          SystemAPI.Query<RefRW<SpawnerReference>, RefRO<SpawnCarShortcut>>().WithAny<SpawnCarShortcut>())
-            // {
-            // }
-            
+            state.RequireForUpdate(_spawnerQuery);
         }
 
         [BurstCompile]
         public void OnDestroy(ref SystemState state)
         {
+            _spawnerQuery.Dispose();
+        }
+
+
+        public void OnUpdate(ref SystemState state)
+        {
+            //Handle car spawn on startup
+            foreach (var spawnerEntity in _spawnerQuery.ToEntityArray(Allocator.Temp))
+            {
+                var entityPrefab = state.EntityManager.GetComponentData<EntityPrefab>(spawnerEntity);
+                var spawnerTransform = state.EntityManager.GetComponentData<LocalTransform>(spawnerEntity);
+                var spawnerColour = state.EntityManager.GetComponentData<ColourComponent>(spawnerEntity);
+                var spawnerPath = state.EntityManager.GetComponentData<PathingComponent>(spawnerEntity);
+            
+                CreateAndUpdateEntity(ref state, entityPrefab, spawnerPath, spawnerTransform, spawnerColour);
+            }
+            
+            if(!_spawnerQuery.IsEmpty)
+                state.EntityManager.SetComponentEnabled<ShouldSpawnOnceMarker>(_spawnerQuery, false);
+            
         }
 
         /**
          * Creates an entity according to the given prefab then update its components
          */
         private void CreateAndUpdateEntity(ref SystemState state, 
-                                           RefRW<EntityPrefab> entityPrefab, 
-                                           RefRO<LocalTransform> spawnerTransform,
-                                           RefRO<ColourComponent> spawnerColour)
+                                           EntityPrefab entityPrefab, 
+                                           PathingComponent spawnerPath,
+                                           LocalTransform spawnerTransform,
+                                           ColourComponent spawnerColour)
         {
             //TODO: create entity appropriately
-            Entity spawnedEntity = state.EntityManager.Instantiate(entityPrefab.ValueRO.prefab);
+            Entity spawnedEntity = state.EntityManager.Instantiate(entityPrefab.prefab);
             state.EntityManager.SetComponentData(spawnedEntity, new LocalTransform
             {
-                Position = new float3(spawnerTransform.ValueRO.Position.x, 0.5f, spawnerTransform.ValueRO.Position.z),
-                Rotation = spawnerTransform.ValueRO.Rotation,
+                Position = new float3(spawnerTransform.Position.x, 0.5f, spawnerTransform.Position.z),
+                Rotation = spawnerTransform.Rotation,
                 Scale = 1
             });
             //Set colour data from the spawner to the entity
-            state.EntityManager.SetComponentData<ColourComponent>(spawnedEntity, new ColourComponent { colour = spawnerColour.ValueRO.colour });
-            //TODO: do the same for path
-            //TODO: check if possible to just reference the spawner and access its components (would allow to link spawner colour/path to the car and prevent duplication)
-
+            state.EntityManager.SetComponentData<ColourComponent>(spawnedEntity, new ColourComponent { colour = spawnerColour.colour });
+            state.EntityManager.SetComponentData<PathingComponent>(spawnedEntity, spawnerPath);
+            //TODO: Try to just reference the spawner and access its components
+            //(would allow to link spawner colour/path to the car and prevent duplication)
         }
     }
 }
